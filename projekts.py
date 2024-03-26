667f3aeecd51774ba60c3c4d896236293f811f20
erikonkuls <152157768+erikonkuls@users.noreply.github.com>
2024-03-26 13:08:54 +0200

    Add files via upload
    pieienots jatajums links utt

--- projekts.py
+++ projekts.py
@@ -11,7 +11,12 @@
 
 #https://www.lsm.lv/raksts/arpus-etera/speles/tests-gudrs-vel-gudraks-izaicina--parbaudi-savas-zinasanas.a434254/
 
+#vai tu esi gatavs testam? jautajumu vajag ieklaut
+#Kādu sarežģītības līmeni tu gribi?
+
+
 from random import randint
+
 
 jautajums = {
 "Cik trilionu km garš ir Gaismas gads (izvēlies burtu) a)5.4  b)9.5  c)8.3  d) 12.9": "B",
@@ -23,24 +28,39 @@ jautajums = {
 " Pirmie gaisa balona pasažieri bija trīs dzīvnieki. Kurš no šiem te neiederas? a) Suns b) Gailis c) Aita d) Pīle " : "A",
 "Uz olimpiskā karoga attēloti pieci apļi. Kādā krāsā nav neviens no apļiem? a) Dzeltenā b) Oranžā c)Zilā d) Melnā":"B",
 "Kurš no šiem terminiem tiek lietots gan medicīnā, gan mūzikā? a)Triole  b)Uzraudzība  c) Bemols  d) Perkusija": "D",
-" Ko pēta malakologi? a) Sliekas  b) Mikroorganismus c) Gliemežus   d) Tauriņus  ":"C"
+" Ko pēta malakologi? a) Sliekas  b) Mikroorganismus c) Gliemežus   d) Tauriņus  ":"C",
+"Cik gadu vecumā Volfgangs Amadejs Mocarts sāka komponēt savus pirmos skaņdarbus? a) 3 gadu b) 5 gadu c) 11 gadu d) 8 gadu":"B",
+"1547, kurš kļuva par 1. Krievijas caru? a) Ivans III Lielais  b) V.Putins  c) Ivans IV  d) Ivans II Skaistais  ":"C",
+"Ja indiešu restorānā pasūtīsiet “murgh” no ēdienkartes, kādu gaļu jūs saņemsiet?  a) Vistu  b) Lielopu  c) Cūkas  d) Mājdzīvnieku " :"A",
+"Kurām sugām starp sauszemes dzīvniekiem ir vislielākās acis? a)Zilonis   b)Žirafe   c)Strauss   d) Tīgeris  " : "C",
+"Kurš ir vienīgais karalis kāršu kavā bez ūsām?  a)Kārava karalis  b)kreica karalis  c)Pīķa karalis  d) Erca karalis " :"D",
+"Kurā valstī katru gadu notiek pasaules čempionāts sievu nēsāšanai a)Norvēģijā  b)Latvijā  c)Igaunijā  d)Somijā ": "D"
+
+
 }
 atslegas= list(jautajums.keys())
+spiedšeit=("{https://ltv.lsm.lv/raidijumi/gudrs-vel-gudraks/pieteikuma-anketa}")
+url=({spiedšeit})
 
 skaits = 0
 pareizas_atbildes = 0
 print("Lai atbildēt uz jautājumu tev ir jāizvēlās viena no dotajām atbildēm ierakstot tikai atbilžu varianta burtu")
+print("lai bēgtu spēli nospied burtu 'q'!")
 while True:
     gadijums = randint(0, len(atslegas)-1)
 
     atbilde = input( {atslegas[gadijums]})
     if atbilde.lower() == "q":
         print("Paldies par spēli!")
+        print("Neaizmirsti pieteikties uz gudrs vēl gudrāks - Dodies uz šo linku!")
+        print(url)
+       
+        
         break
     elif atbilde.capitalize() == jautajums[atslegas[gadijums]]:
         print("pareizi!")
         pareizas_atbildes +=1
     else:
-        print("Ej mācīties!")
+        print("Nepareizi, pareizā atbilde bija "( {atslegas[gadijums]}))
     skaits +=1
 print(f"Tu atbildēji pareizi uz {pareizas_atbildes} / {skaits}")
